c50fe5ca0092697ada401bad05b130c87a2851fa
Kelsang <g.kelsang@yahoo.fr>
2020-11-15 13:28:48 +0100

    afficher le produit en description

--- Frontend/JS/panier.js
+++ Frontend/JS/panier.js
@@ -1,6 +1,87 @@
-// récup ID dans l'URL 
-
-function getID(){
+////////////// Récuprérer l'id dans l'URL /////////////////////////////////////////////////
+function getId(){
     const param = window.location.search;
-    const id = param.replace()
+    const id = param.replace("?id=", ""); // Retire ?ID= des parametres de l'URL, Recupère uniquement l'identitfiant
+    return id;
 }
+///////////////// Ajoute le produit dans le panier avec la lentille sélectionnée par l'utilisateur /////
+function addToBasket(colorSelected){
+    let basketContent = JSON.parse(localStorage.getItem("basketContent"));
+    if (basketContent === null){
+        basketContent = [];
+    }
+
+/////////////////////// Produit ajouter au local storage //////////////////////////////
+    let product = new Product(id, colorSelected);
+
+    basketContent.push(product);
+    localStorage.setItem("basketContent", JSON.stringify(basketContent));
+}
+
+///////////////////////// Ajoute les informations produit dans la page HTLM ///////////////////
+function addProductInfo(response){
+    //création du cadre de l'appareil photo séléctionné
+    const container = document.getElementById("productcontainer");
+
+    const div = document.createElement("div");
+    div.setAttribute("class", "product-border offset-1 col-10 col-md-6 offset-md-3 mt-5 mb-5 p-3 border border-dark");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", response.imageUrl);
+    img.setAttribute("width", "100%");
+
+    const title = document.createElement("div");
+    title.innerHTML = response.name;
+    title.setAttribute("class", "producttitle text-center mb-4");
+
+    const legend = document.createElement("div");
+    legend.innerHTML = response.description;
+    
+    const price = document.createElement("p");
+    price.innerHTML = response.price + "€";
+    
+    // Choix des lentilles
+    const lenses = document.createElement("select");
+    
+    const optionDefault = document.createElement("option");
+    optionDefault.innerHTML = "Merci de choisir une option";
+    lenses.appendChild(optionDefault);
+
+    //alerte ajout panier
+    const btn = document.createElement("button"); 
+    btn.innerHTML = "Ajouter au panier";
+
+    // Ajout d'élément au local storage
+    btn.addEventListener('click', function(){ 
+        const lenses = document.getElementsByTagName("select");         
+        const lenseSelected = lenses[0].value;
+
+        addToBasket(lenseSelected);
+        alert("ajouté au panier");
+    });
+
+    for (let i = 0; i < response.lenses.length; i = i + 1){
+        const option = document.createElement("option");
+        option.setAttribute("value", response.lenses[i]);
+        option.innerHTML = response.lenses[i];
+        lenses.appendChild(option);
+    }
+    // arboresence
+    container.appendChild(div);
+    div.appendChild(title);
+    div.appendChild(img);
+    div.appendChild(legend);
+    div.appendChild(lenses);
+    div.appendChild(price);
+    div.appendChild(btn);
+}
+const id = getId();
+get("http://localhost:3000/api/teddies/" + id).then( function(response){
+    addProductInfo(response);
+    
+}).catch(function(err){
+    console.log(err);
+    if(err === 0){ // requete ajax annulée
+        alert("serveur HS");
+    }
+});
---
+++ Frontend/JS/produit.js
@@ -0,0 +1,43 @@
+////////////// Récuprérer l'id dans l'URL /////////////////////////////////////////////////
+function getId(){
+    const param = window.location.search;
+    const id = param.replace("?id=", ""); // Retire ?ID= des parametres de l'URL, Recupère uniquement l'identitfiant
+    console.log('test')
+    return id;
+}
+
+
+
+///////////////////////// Ajoute les informations produit dans la page HTLM ///////////////////
+function addProductInfo(response){
+    //création du cadre de l'appareil photo séléctionné
+    const container = document.getElementById("productcontainer");
+
+    const div = document.createElement("div");
+    div.setAttribute("class", "product-border offset-1 col-10 col-md-6 offset-md-3 mt-5 mb-5 p-3 border border-dark");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", response.imageUrl);
+    img.setAttribute("width", "100%");
+
+    const title = document.createElement("div");
+    title.innerHTML = response.name;
+    title.setAttribute("class", "producttitle text-center mb-4");
+
+    const legend = document.createElement("div");
+    legend.innerHTML = response.description;
+    
+    const price = document.createElement("p");
+    price.innerHTML = response.price + "€";
+    
+}
+const id = getId();
+get("http://localhost:3000/api/teddies/" + id).then( function(response){
+    addProductInfo(response);
+    
+}).catch(function(err){
+    console.log(err);
+    if(err === 0){ // requete ajax annulée
+        alert("serveur HS");
+    }
+});
