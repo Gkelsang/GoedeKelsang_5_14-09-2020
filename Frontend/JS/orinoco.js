9bb763d6674e24489825fc5c70d2206f4f8bd02d
Kelsang <g.kelsang@yahoo.fr>
2020-11-15 11:54:29 +0100

    affichage produit en dynamique

--- Frontend/JS/orinoco.js
+++ Frontend/JS/orinoco.js
@@ -14,27 +14,13 @@
       console.log(element);
       document.getElementById('item').innerHTML += `
       <div class="card col-4 col-md-4 col-lg-2">
+
       <p class="card-body">ID:${element._id}</p><br>
       <p class="card-title">${element.name}</p>
       <p class="card-text">${element.price} €</p>
       <p><img class="card-img-top" src="${element.imageUrl}"></p> 
-
-lement.name}>
-
-
-
-    
-      
-    
-      
-      `;
-    
-      element.colors.forEach(color => {
-        console.log(color);
-      document.getElementById(element.name).innerHTML += `
-      <option>${color}</option>
+      <p class="card-description">${element.description}</p></p>
       ` 
-    });
     });
   });
   
