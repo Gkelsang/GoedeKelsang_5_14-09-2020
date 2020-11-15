1e769ab9125bd00f16c0071b284fe5204fa3c5ab
Kelsang <g.kelsang@yahoo.fr>
2020-11-15 11:39:39 +0100

    git initial commit

---
+++ Frontend/JS/orinoco.js
@@ -0,0 +1,44 @@
+// récupérer toutes infos de l'API
+
+  fetch('http://localhost:3000/api/teddies')
+
+// récupérer sous forme de données .json
+
+  .then(response => response.json())
+  .then(json =>  {
+    console.log(json)
+
+// Afficher le tout dans une card
+
+    json.forEach(element => {
+      console.log(element);
+      document.getElementById('item').innerHTML += `
+      <div class="card col-4 col-md-4 col-lg-2">
+      <p class="card-body">ID:${element._id}</p><br>
+      <p class="card-title">${element.name}</p>
+      <p class="card-text">${element.price} €</p>
+      <p><img class="card-img-top" src="${element.imageUrl}"></p> 
+
+
+        <select id=${element.name}>
+        </select> <br>
+
+
+    
+      
+    
+      
+      `;
+    
+      element.colors.forEach(color => {
+        console.log(color);
+      document.getElementById(element.name).innerHTML += `
+      <option>${color}</option>
+      ` 
+    });
+    });
+  });
+  
+
+
+  
