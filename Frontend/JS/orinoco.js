298b90ade2d40f54d924295ed53bd545f03186c3
Kelsang <g.kelsang@yahoo.fr>
2020-11-16 13:24:23 +0100

    créaton des items en html

--- Frontend/JS/orinoco.js
+++ Frontend/JS/orinoco.js
@@ -1,46 +1,50 @@
+// --- les infos qui doivent être créer en HTML pour chaques infos récupérer du serveur --- // 
 
-// Ajout des produit en dynamique //
 
 function addProduct(responseProduct, section){
+  const div = document.createElement("div");
+  div.innerHTML = responseProduct.name;
+  div.setAttribute("class", "col-md-5 product-border mt-5 mb-4 col-sm-6 mr-4 ml-4 border border-dark");
 
-    const div = document.createElement("div");
-    div.innerHTML = responseProduct.name;
-    div.setAttribute("class", "col-md-5 product-border mt-5 mb-4 col-sm-6 mr-4 ml-4 border border-dark");
+  const img = document.createElement("img");
+  img.setAttribute("src", responseProduct.imageUrl);
+  img.setAttribute("width", "100%");
 
-    const img = document.createElement("img");
-    img.setAttribute("src", responseProduct.imageUrl);
-    img.setAttribute("width", "100%");
+  const description = document.createElement("div");
+  description.innerHTML = responseProduct.description;
 
-    const description = document.createElement("div");
-    description.innerHTML = responseProduct.description;
+  const colors = document.createElement("p");
+  colors.innerHTML = "Choix des couleurs: "+ responseProduct.colors;
+  
+  const price = document.createElement("p");
+  price.innerHTML = responseProduct.price + "€";
 
-    const colors = document.createElement("p");
-    colors.innerHTML = "Choix des couleurs: "+ responseProduct.colors;
-    
-    const price = document.createElement("p");
-    price.innerHTML = responseProduct.price + "€";
+  const link = document.createElement("a");
+  link.setAttribute("href", "produit.html?id=" + responseProduct._id);
 
-    const link = document.createElement("a");
-    link.setAttribute("href", "produit.html?id=" + responseProduct._id);
-
-    section[1].appendChild(div);
-    div.appendChild(link);
-    link.appendChild(img);
-    div.appendChild(description);
-    div.appendChild(colors);
-    div.appendChild(price);
+  section[1].appendChild(div);
+  div.appendChild(link);
+  link.appendChild(img);
+  div.appendChild(description);
+  div.appendChild(colors);
+  div.appendChild(price);
 }
+
+// --- Création d'une div pour intégrer les items --- // 
 
 function addDivToFixDisplay(section){
-    const div = document.createElement("div");
-    div.setAttribute("class", "col-md-5 mt-5 mb-4 ml-4 mr-4");
-    section[1].appendChild(div);
+  const div = document.createElement("div");
+  div.setAttribute("class", "col-md-5 mt-5 mb-4 ml-4 mr-4");
+  section[1].appendChild(div);
 }
 
+
 get("http://localhost:3000/api/teddies").then( function(response){
-    const section = document.getElementsByClassName("row");
-    
-    for (let i = 0; i < response.length; i = i + 1){   
-        addProduct(response[i], section);
-    }
+  const section = document.getElementsByClassName("item");
+  
+  // --- Création de la card --- // 
+
+  for (let i = 0; i < response.length; i = i + 1){   
+      addProduct(response[i], section);
+  }
 });
--- Frontend/JS/produit.js
+++ Frontend/JS/produit.js
@@ -4,16 +4,19 @@ function getId(){
     const id = param.replace("?id=", ""); // Retire ?ID= des parametres de l'URL, Recupère uniquement l'identitfiant
     return id;
 }
-///
+///////////////// Ajoute le produit dans le panier avec la lentille sélectionnée par l'utilisateur /////
+function addToBasket(lenseSelected){
+    let basketContent = JSON.parse(localStorage.getItem("basketContent"));
+    if (basketContent === null){
+        basketContent = [];
+    }
 
+/////////////////////// Produit ajouter au local storage //////////////////////////////
+    let product = new Product(id, lenseSelected);
 
-
-
-
-
-
-
-
+    basketContent.push(product);
+    localStorage.setItem("basketContent", JSON.stringify(basketContent));
+}
 
 ///////////////////////// Ajoute les informations produit dans la page HTLM ///////////////////
 function addProductInfo(response){
@@ -37,16 +40,48 @@ function addProductInfo(response){
     const price = document.createElement("p");
     price.innerHTML = response.price + "€";
     
+    // Choix des lentilles
+    const lenses = document.createElement("select");
+    
+    const optionDefault = document.createElement("option");
+    optionDefault.innerHTML = "Merci de choisir une option";
+    lenses.appendChild(optionDefault);
+
+    //alerte ajout panier
+    const btn = document.createElement("button"); 
+    btn.innerHTML = "Ajouter au panier";
+
+    // Ajout d'élément au local storage
+    btn.addEventListener('click', function(){ 
+        const lenses = document.getElementsByTagName("select");         
+        const lenseSelected = lenses[0].value;
+
+        addToBasket(lenseSelected);
+        alert("ajouté au panier");
+    });
+
+    for (let i = 0; i < response.lenses.length; i = i + 1){
+        const option = document.createElement("option");
+        option.setAttribute("value", response.lenses[i]);
+        option.innerHTML = response.lenses[i];
+        lenses.appendChild(option);
+    }
     // arboresence
     container.appendChild(div);
     div.appendChild(title);
     div.appendChild(img);
-    div.appendChild(description);
-    div.appendChild(colors);
+    div.appendChild(legend);
+    div.appendChild(lenses);
     div.appendChild(price);
     div.appendChild(btn);
 }
 const id = getId();
-get("http://localhost:3000/api/teddies/" + id).then( function(response){
+get("http://localhost:3000/api/cameras/" + id).then( function(response){
     addProductInfo(response);
+    
+}).catch(function(err){
+    console.log(err);
+    if(err === 0){ // requete ajax annulée
+        alert("serveur HS");
+    }
 });
