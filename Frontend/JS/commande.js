7531e5f115e008909c1728cef89dc8d6ee172e43
Kelsang <g.kelsang@yahoo.fr>
2020-11-21 14:27:37 +0100

    mise en page

--- Frontend/JS/commande.js
+++ Frontend/JS/commande.js
@@ -1,14 +1,25 @@
 function addConfirmationText(){
-    const confirmationId = localStorage.getItem("orderConfirmation"); // récuperation de l'ID dans le local storage
-    const totalPrice = localStorage.getItem("totalPriceConfirmationPage"); //récuperation du prix dans le local storage
+
+    // --- récupere le numéro de la commande dans le local storage --- //
+    const confirmationId = localStorage.getItem("orderConfirmation");
+    // --- récupere la balise dans laquelle afficher le numéro de commande --- //
     const confirmation = document.getElementById("confirmation");
+    // --- Crée un <p> en html avec le numéro de commande --- //
     const messageConfirmation = document.createElement("p");
-    const confirmationPrice = document.createElement("p");
-    messageConfirmation.innerHTML = "votre commande n° "+ confirmationId;
-    confirmationPrice.innerHTML = "Prix total: "+ totalPrice + "€";
-    messageConfirmation.setAttribute("class", "confirmation-title pt-5")
+    messageConfirmation.innerHTML = confirmationId;
+    messageConfirmation.setAttribute("class", "confirmation-title pt-3")
     confirmation.appendChild(messageConfirmation);
-    confirmation.appendChild(confirmationPrice);
+
+    // --- récupere le total du prix dans le local storage --- //
+    const totalPrice = localStorage.getItem("totalPriceConfirmationPage");
+    // --- récupere la balise dans laquelle afficher le numéro de commande --- //
+    const prix = document.getElementById("prix");
+    // --- Crée un <p> en html avec le numéro de commande --- //
+    const confirmationPrice = document.createElement("p");
+    confirmationPrice.innerHTML = totalPrice + "€";
+    confirmationPrice.setAttribute("class", "confirmation-title pt-3")
+    prix.appendChild(confirmationPrice);
+
 }
 
 addConfirmationText();
